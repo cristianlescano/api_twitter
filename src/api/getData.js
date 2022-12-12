be48a9ba9482c527d147b40f03980afab0cb3fea
assanchez22 <sanchezaugustosebastian6@gmail.com>
2022-12-12 10:21:47 -0300

    cambios

--- src/api/getData.js
+++ src/api/getData.js
@@ -8,10 +8,11 @@ const readOnlyClient = twitterClient.readOnly;
 
 const user = await readOnlyClient.v2.userByUsername("cryptob_chain");
 
-console.log(user.data.id);
+console.log('El ID del usuario es: ' + user.data.id);
 
 const todosLosTweets = await readOnlyClient.v2.userTimeline(user.data.id, {
   exclude : "replies",
+  exclude : "retweets",
   expansions: 'attachments.media_keys',
   'media.fields': 'url',
 });
@@ -20,10 +21,10 @@ let tweets = [];
 
 for (const unTweet of todosLosTweets) {
   let link = ''
-  unTweet.attachments?.media_keys.forEach ((data12) => {
-    todosLosTweets.data.includes.media.forEach ((data25) => {
-      if(data12 == data25.media_key) {
-        return link = data25.url
+  unTweet.attachments?.media_keys.forEach ((date_1) => {
+    todosLosTweets.data.includes.media.forEach ((date_2) => {
+      if(date_1 == date_2.media_key) {
+        return link = date_2.url
       }
     }) 
     unTweet.url = link 
